b612d4fc4491df52acc715ac4f9d77bc6eb51bee
Jayjay <jayjayish@gmail.com>
2019-02-16 10:31:03 -0800

    A bunch of scripts that dont work atm

---
+++ HotFall/Assets/Scripts/Tools/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities {
+
+    public static Vector3 worldToScreenObjectPosition(GameObject gObj)
+    {
+        return Camera.main.WorldToScreenPoint(gObj.transform.position);
+    }
+
+    public static float getAngleDegBetween(float y, float x)
+    {
+        return Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+    }
+
+    public static float getAngleDegBetweenMouseAnd(GameObject gObj)
+    {
+        Vector3 dir = directionBetweenMouseAndCharacter(gObj);
+        return getAngleDegBetween(dir.y, dir.x);
+    }
+
+    private static Vector3 directionBetweenMouseAndCharacter(GameObject gObj)
+    {
+        return Input.mousePosition - worldToScreenObjectPosition(gObj);
+    }
+}
