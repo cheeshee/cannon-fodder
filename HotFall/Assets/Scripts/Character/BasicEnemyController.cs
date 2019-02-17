f42e4f35e42bd310390fe5a36b03255ec0be5844
Jayjay <jayjayish@gmail.com>
2019-02-17 02:13:07 -0800

    Everything merged and new basic enemy

---
+++ HotFall/Assets/Scripts/Character/BasicEnemyController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : EnemyController
+{
+    public override void updateDestination()
+    {
+        base.destination = new Vector2(0f, 0f);
+    }
+}
---
+++ HotFall/Assets/Scripts/Character/EnemyController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+public class EnemyController : ICharacter, IPooledObject {
+
+    const string ANIMATION_DEATH = "Death";
+    const string ANIMATION_DAMAGED = "Damaged";
+
+    protected GameObject player;
+
+    [SerializeField]
+    protected float speedMultiplier = 1;
+
+    public Vector2 destination;
+
+    float step;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player = GameObject.FindGameObjectWithTag(Tags.PLAYER);
+    }
+
+    public void OnObjectSpawn()
+    {
+        base.healthPoints = base.maxHealth;
+        base.updateHealthBar();
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        setSpeed();
+        updateDestination();
+        move();
+        //updateSpriteDirection();
+    }
+
+
+
+
+    #region Motion
+    void setSpeed()
+    {
+        if (isWithinSpeedUp())
+        {
+            float speedIncrease = base.speedModifier * (1 + speedMultiplier * player.GetComponent<Rigidbody2D>().velocity.magnitude);
+            step = Mathf.Min(speedIncrease * Time.deltaTime, 3 * base.SpeedModifier() * Time.deltaTime);
+        }
+        else
+        {
+            step = base.SpeedModifier() * Time.deltaTime;
+        }
+    }
+
+    private void move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destination, step);
+    }
+
+    private bool isWithinSpeedUp()
+    {
+        return Vector3.Distance(player.transform.position, transform.position) - player.GetComponent<CircleCollider2D>().radius <= player.GetComponent<PlayerController>().getSpeedUpRange();
+    }
+
+    public virtual void updateDestination()
+    {
+        destination = player.transform.position;
+    }
+
+    /*
+    void updateSpriteDirection()
+    {
+        // not sure if this is right
+        transform.localScale = gameObject.transform.position;
+    }
+    */
+    #endregion
+
+    #region LifeDeath
+
+    protected override void onDeath()
+    {
+        onCharacterDeath(this);
+
+        gameObject.SetActive(false);
+        
+        // runAnimation(ANIMATION_DEATH);
+        //Invoke("completeDeathAnimation", 1.5f);
+        
+    }
+
+    void runAnimation(string name)
+    {
+        //GetComponent<Animator>().SetTrigger(name);
+    }
+
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        GameObject player = other.gameObject;
+
+        if (player.tag == Tags.PLAYER)
+        {
+            //player.GetComponent<ICharacter>().damagedByAttacker(meleeDamage);
+        }
+    }
+
+    #endregion
+}
