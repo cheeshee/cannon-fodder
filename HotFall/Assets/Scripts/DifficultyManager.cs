274a64c6026974f5ae561751ca8f3bd6be23cf43
Jayjay <jayjayish@gmail.com>
2019-02-17 14:50:09 -0800

    asfdsadf

--- HotFall/Assets/Scripts/DifficultyManager.cs
+++ HotFall/Assets/Scripts/DifficultyManager.cs
@@ -8,18 +8,34 @@ public class DifficultyManager : MonoBehaviour
     GameObject Spawner;
 
     private SpawnManager SpawnScript;
-
+    private SpawnManager.Enemies basic;
+    private SpawnManager.Enemies zero;
+    private SpawnManager.Enemies spiral;
     // Start is called before the first frame update
     void Start()
     {
         SpawnScript = Spawner.GetComponent<SpawnManager>();
-        SpawnManager.Enemies yes = SpawnScript.monstersToSpawn[0];
-        yes.percentage = 0;
+        SpawnManager.Enemies basic = SpawnScript.monstersToSpawn[0];
+        SpawnManager.Enemies zero = SpawnScript.monstersToSpawn[1];
+        SpawnManager.Enemies spiral = SpawnScript.monstersToSpawn[2];
+        basic.percentage = 0;
+        zero.percentage = 1;
+        spiral.percentage = 0;
+        SpawnScript.setMaxEnemies(5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time >= 5 && Time.time <= 6)
+        {
+            SpawnManager.Enemies basic = SpawnScript.monstersToSpawn[0];
+            SpawnManager.Enemies zero = SpawnScript.monstersToSpawn[1];
+            SpawnManager.Enemies spiral = SpawnScript.monstersToSpawn[2];
+            basic.percentage = 0.2f;
+            zero.percentage = 1;
+            spiral.percentage = 0;
+            SpawnScript.setMaxEnemies(20);
+        }
     }
 }
